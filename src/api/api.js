29b51af58d1539184f50ae26c5d6c1720ffdf4f3
zt <zt@ztdeMacBook-Pro.lan>
2018-11-05 23:02:41 +0800

    添加功能

--- src/api/api.js
+++ src/api/api.js
@@ -6,9 +6,6 @@ import { wxRequest } from "@/util/util";
 const getHotTripList = (params) => {
   return wxRequest(params, `${apiURL}/v2/index/`);
 };
-const getExplorePlaceList = (params) => {
-  return wxRequest(params, `${apiURL}/destination/v3/`);
-};
 // 通过ID获取地点相关信息
 const getPlaceInfoByID = (params) => {
   return wxRequest(params, `${apiURL}/destination/place/5/${params.query.id}/`);
@@ -21,40 +18,24 @@ const getPlaceTipsByID = (params) => {
 const getPlacePhotosByID = (params) => {
   return wxRequest(params, `${apiURL}/destination/place/5/${params.query.id}/photos/`);
 };
-const getPlacePOIByID = (params) => {
-  return wxRequest(params, `${apiURL}/destination/place/${params.query.type}/${params.query.id}/pois/${params.query.poiType}/`);
-};
 // 通过ID获取旅行日记信息
 const getTripInfoByID = (params) => {
   return wxRequest(params, `${apiURL}/trips/${params.query.tripId}/waypoints/`);
-};
-const getPlaceTripByID = (params) => {
-  return wxRequest(params, `${apiURL}/destination/place/${params.query.type}/${params.query.id}/trips/`);
 };
 // 通过ID获取所有旅行地点
 const getPlacesByID = (params) => {
   return wxRequest(params, `${apiURL}/trips/${params.query.tripId}/schedule/`);
 };
+// 通过用户ID获取用户信息
 const getUserInfoByID = (params) => {
   return wxRequest(params, `${apiURL}/users/${params.query.userId}/v2`);
-};
-const getWaypointInfoByID = (params) => {
-  return wxRequest(params, `${apiURL}/trips/${params.query.tripId}/waypoints/${params.query.waypointId}/`);
-};
-const getWaypointReplyByID = (params) => {
-  return wxRequest(params, `${apiURL}/trips/${params.query.tripId}/waypoints/${params.query.waypointId}/replies/`);
 };
 
 module.exports = {
   getHotTripList,
-  getExplorePlaceList,
   getPlaceInfoByID,
-  getPlacePOIByID,
   getTripInfoByID,
-  getPlaceTripByID,
   getUserInfoByID,
-  getWaypointInfoByID,
-  getWaypointReplyByID,
   getPlacesByID,
   getPlaceTipsByID,
   getPlacePhotosByID
